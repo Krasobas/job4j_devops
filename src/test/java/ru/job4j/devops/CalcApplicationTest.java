fd34c7adadd8cd39f238eadc1b3ea272d78b1285
v.krasov.ext <v.krasov@aleria.cloud>
2026-03-23 22:57:53 +0400

    add test

--- src/test/java/ru/job4j/devops/CalcApplicationTest.java
+++ src/test/java/ru/job4j/devops/CalcApplicationTest.java
@@ -29,4 +29,13 @@ class CalcApplicationTests {
         assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
         assertThat(output.getBody()).isEqualTo(expected);
     }
+
+    @Test
+    public void whenZero() {
+        var input = new TwoArgs(0, 0);
+        var expected = new Result(0);
+        var output = new CalcController().summarise(input);
+        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(output.getBody()).isEqualTo(expected);
+    }
 }
