7a8773eca63e790568f7557678bfa39b70f15de2
v.krasov.ext <v.krasov@aleria.cloud>
2026-04-03 00:52:17 +0400

    add optimise testcontainers

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,2 @@
 rootProject.name = "DevOps"
-include("DevOps.intergrationTest")
-include("intergrationTest")
-include("src:intergrationTest")
-findProject(":src:intergrationTest")?.name = "intergrationTest"
+include("src:integrationTest")
--- src/integrationTest/java/ru/job4j/devops/listener/CalcEventListenerTest.java
+++ src/integrationTest/java/ru/job4j/devops/listener/CalcEventListenerTest.java
@@ -11,7 +11,9 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
+import ru.job4j.devops.config.ContainersConfig;
 import ru.job4j.devops.models.User;
+import ru.job4j.devops.repository.UserRepository;
 import ru.job4j.devops.service.CalcService;
 
 import java.time.Duration;
@@ -21,41 +23,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
-class CalcEventListenerTest {
-    private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(
-        "postgres:16-alpine"
-    );
-
-    private static final KafkaContainer KAFKA = new KafkaContainer(
-        DockerImageName.parse("apache/kafka:3.7.2")
-    );
-
+class CalcEventListenerTest extends ContainersConfig {
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Autowired
     private CalcService calcService;
-
-    @BeforeAll
-    static void beforeAll() {
-        POSTGRES.start();
-        KAFKA.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        POSTGRES.stop();
-        KAFKA.stop();
-    }
-
-    @DynamicPropertySource
-    public static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.consumer.auto-offset-reset", () -> "earliest");
-        registry.add("spring.kafka.bootstrap-servers", KAFKA::getBootstrapServers);
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
-    }
 
     @Test
     public void whenAddAsyncThenSaveEventIntoDb() {
