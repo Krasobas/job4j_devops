45d4be25efeb02a481251ac218b2a73ee4bf8a41
v.krasov.ext <v.krasov@aleria.cloud>
2026-04-17 02:38:23 +0400

    add custom metrics

--- src/main/java/ru/job4j/devops/service/ResultDbService.java
+++ src/main/java/ru/job4j/devops/service/ResultDbService.java
@@ -1,5 +1,9 @@
 package ru.job4j.devops.service;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.job4j.devops.models.Result;
@@ -11,13 +15,25 @@ import java.util.List;
 @AllArgsConstructor
 public class ResultDbService implements ResultService {
     private final ResultRepository resultRepository;
+    private final MeterRegistry meterRegistry;
+
+    private Counter saveCounter;
+    private Timer saveTimer;
+
+    @PostConstruct
+    public void init() {
+        this.saveCounter = meterRegistry.counter("results.save");
+        this.saveTimer = meterRegistry.timer("results.save.timer");
+    }
 
     public void save(Result result) {
-        resultRepository.save(result);
+        saveTimer.record(() -> {
+            resultRepository.save(result);
+        });
+        saveCounter.increment();
     }
 
     public List<Result> findAll() {
         return resultRepository.findAll();
     }
 }
-
