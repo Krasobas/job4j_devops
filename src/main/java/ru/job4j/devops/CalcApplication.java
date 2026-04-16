6a11a06604753715a4bcfe7de9c0e8530b2d4a8f
v.krasov.ext <v.krasov@aleria.cloud>
2026-04-17 00:38:57 +0400

    test error log

--- src/main/java/ru/job4j/devops/CalcApplication.java
+++ src/main/java/ru/job4j/devops/CalcApplication.java
@@ -1,12 +1,14 @@
 package ru.job4j.devops;
 
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Main class
  */
+@Log4j2
 @SpringBootApplication
 public class CalcApplication {
 	/**
@@ -21,5 +23,18 @@ public class CalcApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CalcApplication.class, args);
+		new Thread(
+			() -> {
+				while (true) {
+					log.error("Check");
+					log.error("NPE");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						throw new RuntimeException(e);
+					}
+				}
+			}
+		).start();
 	}
 }
